eabff8f25cf4709f4a1de39f99641e0ded07d1ba
Katarzyna Pruś <kasia.prus125@gmail.com>
2023-03-30 23:51:47 +0200

    Add js code for game

--- js/script.js
+++ js/script.js
@@ -4,15 +4,15 @@ console.log('Wylosowana liczba to: ' + randomNumber);
 
 let computerMove = 'nieznany ruch';
 
-if(randomNumber == 1){
+if( randomNumber == 1){
   computerMove = 'kamień';
 }
 
-else if(randomNumber == 2){
+else if( randomNumber == 2){
     computerMove = 'papier';
 }
 
-else if(randomNumber == 3) {
+else if( randomNumber == 3) {
     computerMove = 'nożyce';
 }
 
@@ -25,16 +25,56 @@ console.log('Gracz wpisał: ' + playerInput);
 
 let playerMove = 'nieznany ruch';
 
-if(playerInput == '1'){
+if( playerInput == '1'){
   playerMove = 'kamień';
 }
 
-else if(playerInput == '2'){
+else if( playerInput == '2'){
     playerMove = 'papier';
 }
 
-else if(playerInput == '3'){
+else if( playerInput == '3'){
     playerMove = 'nożyce';
 }
 
 printMessage('Twój ruch to: ' + playerMove);
+
+if( computerMove == 'kamień' && playerMove == 'papier'){
+    printMessage('Ty wygrywasz!');
+}
+
+else( computerMove == 'papier' && playerMove == 'kamień'){
+    printMessage('Ty przegrywasz');
+}
+
+if( computerMove == 'nożyce' && playerMove == 'kamień'){
+    printMessage('Ty wygrywasz');
+}
+
+else( computerMove == 'kamień' && playerMove == 'nożyce'){
+    printMessage('Ty przegrywasz');
+}
+
+if( computerMove == 'papier' && playerMove == 'nożyce'){
+    printMessage('Ty wygrywasz');
+}
+
+else( computerMove == 'nożyce' && playerMove == 'papier'){
+    printMessage('Ty przegrywasz');
+}
+
+if( computerMove == 'kamień' && playerMove == 'kamień'){
+    printMessage('Remis');
+}
+
+if( computerMove == 'papier' && playerMove == 'papier'){
+    printMessage('Remis');
+}
+
+if( computerMove == 'nożyce' && playerMove == 'nożyce'){
+    printMessage('Remis');
+}
+
+if( computerMove == "nożyce" && playerMove == "nieznany ruch"){
+    printMessage('Walkower');
+}
